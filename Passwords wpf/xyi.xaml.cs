b6c840a4062b834c3ca316d42a0f64b2ddc48de8
Avdoshkammm <Avdonin_mv@mail.ru>
2022-09-25 05:21:19 +0300

    Добавьте файлы проекта.

---
+++ Passwords wpf/xyi.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Passwords_wpf
+{
+    /// <summary>
+    /// Логика взаимодействия для xyi.xaml
+    /// </summary>
+    public partial class xyi : Window
+    {
+        public xyi()
+        {
+            InitializeComponent();
+            string combo_1;
+            //тут должен быть вывод текста, но я не ебу как его вывести. Типо должно выводиться на новую форму логин и пароль например "админ админ"
+        }
+    }
+}
